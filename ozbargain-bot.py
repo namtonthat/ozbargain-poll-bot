66bc2a47a6c2b879a0205e7b2d7126ff255cfe38
Nam Tonthat <n.nam.tonthat@gmail.com>
2022-11-30 16:52:43 +1100

    Update markdown and generate Github index page

--- ozbargain-bot.py
+++ ozbargain-bot.py
@@ -41,11 +41,46 @@ def find_all_active_polls():
     return poll_ids
 
 
+def create_bokeh_plot(options, votes, title, id):
+    "Generate bokeh pie chart"
+    LOGGER.info('Data found for %s', id)
+    x = dict(zip(options, votes))
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'options'})
+    data['angle'] = data['value']/data['value'].sum() * 2*pi
+    if len(x) > 2:
+            data['color'] = TolRainbow[len(x)]
+    elif (len(x) > 0) and (len(x) <=2):
+            data['color'] = Sunset[3][:len(x)]
 
+    # checks whether an options exists
+    # set theme
+    curdoc().theme='light_minimal'
+
+    # create figure
+    p = figure(width=1000, height=1000, title=f"{title}",
+            tooltips="@options: @value", x_range=(-0.5, 1.0), name='pie')
+
+
+    p.wedge(x=0, y=1, radius=0.4,
+            start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+            line_color="white", fill_color='color', legend_field='options', source=data)
+
+    p.axis.axis_label = None
+    p.axis.visible = False
+    p.grid.grid_line_color = None
+    p.title.text_font='tahoma'
+
+    LOGGER.info('Generating webchart for poll: %s', id)
+
+    # setting output
+    output_file(filename=f"{OUTPUT_PATH}/{id}.html", title=title)
+
+    save(p)
+
+    return
 def generate_poll_webchart(id):
     "Generates a pie chart for a given poll"
-    prefix_url = "https://www.ozbargain.com.au/node/"
-    url = prefix_url + str(id)
+    url = PREFIX_URL + str(id)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     poll = soup.find(id="poll")
@@ -57,51 +92,20 @@ def generate_poll_webchart(id):
             options = [option.get_text() for option in span_options]
             votes = [int(vote.get_text()) for vote in span_vote]
             title = soup.find("title").text.split(" - ")[0]
+            if options:
+                create_bokeh_plot(options, votes, title, id)
+                return title,id
+
     except AttributeError:
             LOGGER.info('No data found for %s', id)
             return
 
-    if options:
-            LOGGER.info('Data found for %s', id)
-            x = dict(zip(options, votes))
-            data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'options'})
-            data['angle'] = data['value']/data['value'].sum() * 2*pi
-            if len(x) > 2:
-                    data['color'] = TolRainbow[len(x)]
-            elif (len(x) > 0) and (len(x) <=2):
-                    data['color'] = Sunset[3][:len(x)]
+def create_html_page(title_and_id_array):
+    """ Create navigation webpage for all available Ozbargain Polls within the last day"""
+    markdown_text = "## Ozbargain Active Polls (within the last 24 hours)\n\n"
 
-            # checks whether an options exists
-            # set theme
-            curdoc().theme='light_minimal'
-
-            # create figure
-            p = figure(width=1000, height=1000, title=f"{title}",
-                    tooltips="@options: @value", x_range=(-0.5, 1.0))
-
-
-            p.wedge(x=0, y=1, radius=0.4,
-                    start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
-                    line_color="white", fill_color='color', legend_field='options', source=data)
-
-            p.axis.axis_label = None
-            p.axis.visible = False
-            p.grid.grid_line_color = None
-
-
-
-            LOGGER.info('Generating webchart for poll: %s', id)
-            # setting output
-            output_file(filename=f"{OUTPUT_PATH}/{id}.html", title=title)
-
-            save(p)
-            return id
-
-def create_html_page(array):
-    """ Render HTML index page for navigation """
-    markdown_text = ""
-    for value in array:
-        markdown_text+=f"* <a href='{value}.html'>{value}</a>\n"
+    for (title, value) in title_and_id_array:
+        markdown_text+=f"* <a href='{PREFIX_URL}/{value}' title='Ozbargain Poll'>({value})</a> <a href='{value}.html' title='Rendered Pie Chart'>{title}</a>\n"
 
     html_page = markdown2.markdown(markdown_text)
     index_path = f"{OUTPUT_PATH}/index.html"
@@ -112,14 +116,22 @@ def create_html_page(array):
     return
 
 if __name__ == "__main__":
-    OUTPUT_PATH = f"dist/"
+    # global variables
+    PREFIX_URL = "https://www.ozbargain.com.au/node/"
+    OUTPUT_PATH = "dist/"
+
+    # create paths
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
+    # find active polls
     active_polls = find_all_active_polls()
 
-    pie_chart_ids = []
+
+    pie_charts = []
     for poll_id in active_polls:
-        pie_chart_id = generate_poll_webchart(poll_id)
-        if pie_chart_id:
-            pie_chart_ids.append(pie_chart_id)
+        pie_chart = generate_poll_webchart(poll_id)
+        pie_charts.append(pie_chart)
+
+
+    create_html_page(pie_charts)
